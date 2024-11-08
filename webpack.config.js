580edb352139027dae7d2a6f8f2aef64d0ce9415
Alex Christian <alexchristianqr@gmail.com>
2024-11-08 13:43:54 -0500

    agregado webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './dist/lambda.js', // Archivo principal dentro de la carpeta dist
+  target: 'node', // Para aplicaciones Node.js
+  externals: [nodeExternals()], // Excluye node_modules del bundle
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'build'), // Carpeta de salida
+    filename: 'webpack.js', // Archivo generado en la carpeta dist
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'dist', to: 'dist' }, // Copia el directorio dist al directorio de salida
+        { from: '.env', to: '.' }, // Copia el archivo .env al directorio de salida
+      ],
+    }),
+  ],
+};
